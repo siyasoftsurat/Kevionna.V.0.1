01472e5931f0f449ee6b037273d4e2aa6cd88b93
riteshmistry <ritesh@gmail.com>
2019-09-23 17:56:15 +0530

    update

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
 Route::group(['middleware' => 'auth:api'], function(){
 		Route::post('details', 'API\UserController@details');
+		Route::post('details', 'API\CategoryController@store');
 });
