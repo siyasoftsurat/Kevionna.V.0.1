184d76ceb6011509f41681aa897e4bcbd8253d75
Jitendra <JinayAahir>
2019-09-24 09:39:12 +0530

    jinay Commit

--- app/Http/Controllers/API/CompanyInfoController.php
+++ app/Http/Controllers/API/CompanyInfoController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Category;
+use App\CompanyInfo;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use Validator;
 
-class CategoryController extends Controller
+class CompanyInfoController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -43,10 +41,10 @@ class CategoryController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Category  $category
+     * @param  \App\CompanyInfo  $companyInfo
      * @return \Illuminate\Http\Response
      */
-    public function show(Category $category)
+    public function show(CompanyInfo $companyInfo)
     {
         //
     }
@@ -54,10 +52,10 @@ class CategoryController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Category  $category
+     * @param  \App\CompanyInfo  $companyInfo
      * @return \Illuminate\Http\Response
      */
-    public function edit(Category $category)
+    public function edit(CompanyInfo $companyInfo)
     {
         //
     }
@@ -66,10 +64,10 @@ class CategoryController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Category  $category
+     * @param  \App\CompanyInfo  $companyInfo
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Category $category)
+    public function update(Request $request, CompanyInfo $companyInfo)
     {
         //
     }
@@ -77,10 +75,10 @@ class CategoryController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Category  $category
+     * @param  \App\CompanyInfo  $companyInfo
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Category $category)
+    public function destroy(CompanyInfo $companyInfo)
     {
         //
     }
--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,7 @@ Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
 Route::group(['middleware' => 'auth:api'], function(){
 		Route::post('details', 'API\UserController@details');
-
 		Route::get('logout', 'API\UserController@logout');
-
 		Route::post('details', 'API\CategoryController@store');
 
 });
